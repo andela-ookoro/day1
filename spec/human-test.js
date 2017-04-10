eb19cff93f2ffd0c254e237b09ad5e3320d2e9bc
celestine <okorocelestine@gmail.com>
2017-04-10 09:59:28 +0100

    sanitize user input

---
+++ spec/human-test.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+    var   Human = require('../lib/human.js');
+  describe("Determine the sequence of an array of numbers: ", function() {
+
+    describe("Case for an empty array", function() {
+
+      it("should return 0 for an empty array", function() {
+        expect(aritGeo([])).toEqual(0);
+      });
+
+    });
+
+    
+    });
+  });
+})();
