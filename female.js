18325aa1f6c03ce5b0cf95cfe9ace3684d645935
celestine <okorocelestine@gmail.com>
2017-04-10 08:53:50 +0100

    add marry function

--- female.js
+++ female.js
@@ -1,20 +1,18 @@
 import Human from './Human.js';
 
 
-class female extends Human {
+class Female extends Human {
     constructor (_name,_bio,_dob= '') {
     	  super(_name,_dob= '',_bio,'Female');
-    	  this.isPregnant = false;
-    	  this.Pre
+          this.isPregenant = false;
       }
 
     pregenant(_howLong) {
-    	this.isPregenant = true;
     	if ( !this.takeInLog)
     		this.takeInLog = [];
-    	var takeinDate = new Date();
-    	this.takeInLog.push()
-
+    	var takeinDate = new Date() - _howLong;
+    	this.takeInLog.push(takeinDate);
+        if ( (takeinDate + 270) > getDate()  ) ? this.isPregnant = true : this.isPregnant = false;
     }
 
   }
