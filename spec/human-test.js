e0343fc38091710eb1aa60c6d391c03db3ee1550
celestine <okorocelestine@gmail.com>
2017-04-10 10:16:51 +0100

    add test for sayYourName

--- spec/human-test.js
+++ spec/human-test.js
@@ -1,17 +1,16 @@
 (function(){
   'use strict';
     var   Human = require('../lib/human.js');
-  describe("Determine the sequence of an array of numbers: ", function() {
+  describe("Desmonstrata basic human features: ", function() {
 
-    describe("Case for an empty array", function() {
+    describe("Case Invalid input", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(aritGeo([])).toEqual(0);
+        let Cele = new Human('okoro celestine okwudiri','I exist as tool of positive change','male','24/11/1999');
+         expect(Cele.sayYourName()).toEqual('I am Okoro Celestine Okwudiri ');
       });
 
     });
 
-    
-    });
   });
 })();
