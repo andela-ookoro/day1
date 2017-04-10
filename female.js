33821cfff8a3ab66a2c9086d54e260fa9a1ad52b
celestine <okorocelestine@gmail.com>
2017-04-10 08:25:38 +0100

    create the female subclass

---
+++ female.js
@@ -0,0 +1,20 @@
+import Human from './Human.js';
+
+
+class female extends Human {
+    constructor (_name,_bio,_dob= '') {
+    	  super(_name,_dob= '',_bio,'Female');
+    	  this.isPregnant = false;
+    	  this.Pre
+      }
+
+    pregenant(_howLong) {
+    	this.isPregenant = true;
+    	if ( !this.takeInLog)
+    		this.takeInLog = [];
+    	var takeinDate = new Date();
+    	this.takeInLog.push()
+
+    }
+
+  }
